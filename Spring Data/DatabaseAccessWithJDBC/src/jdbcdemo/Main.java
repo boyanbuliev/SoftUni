1006686d23f817c3e3b9bd2b5f31155e97662e0a
Boyan Buliev <boyanbot@gmail.com>
2020-12-16 22:41:26 +0200

    setup DB demo

---
+++ Spring Data/DatabaseAccessWithJDBC/src/jdbcdemo/Main.java
@@ -0,0 +1,60 @@
+package jdbcdemo;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Main {
+    public static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static String DB_URL = "jdbc:mysql://localhost:3306/soft_uni";
+    public static String SQL_QUERY = "SELECT * FROM employees WHERE salary > ?";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter DB username ('root'): ");
+        String username = sc.nextLine().trim();
+        username = username.length() > 0 ? username : "root";
+        System.out.println("Enter DB password ('1234'): ");
+        String password = sc.nextLine().trim();
+        password = password.length() > 0 ? username : "1234";
+
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.err.printf("Database driver: %s not found.", DB_DRIVER);
+            System.exit(0);
+        }
+        System.out.println("DB Driver loaded successfully");
+
+        System.out.println("Enter minimal salary ('40000'): ");
+        String salaryStr = sc.nextLine();
+        salaryStr = salaryStr.length() > 0 ? salaryStr : "40000";
+        double salary = 40000;
+        try {
+            salary = Double.parseDouble(salaryStr);
+        } catch (NumberFormatException ex) {
+            System.err.printf("Invalid number: %s", salaryStr);
+        }
+        Properties properties = new Properties();
+        properties.setProperty("user", username);
+        properties.setProperty("password", password);
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, properties);
+             PreparedStatement ps = connection.prepareStatement(SQL_QUERY)) {
+            System.out.printf("DB connection created successfully: %s%n", DB_URL);
+
+            ps.setDouble(1, salary);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                System.out.printf("| %10d | %-15.15s | %-15.15s | %10.2f |%n",
+                        rs.getLong("employee_id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getDouble("salary"));
+            }
+        } catch (SQLException throwables) {
+            System.err.printf("Error closing DB connection %s", throwables.getMessage());
+        }
+    }
+}
