abae0d798fd380bd636dab49fcf4bf3f3d739d68
Boyan Buliev <boyanbot@gmail.com>
2020-12-18 21:12:24 +0200

    Basic ORM created

---
+++ Spring Data/ORMFundamentals/src/main/java/orm/Connection.java
@@ -0,0 +1,22 @@
+package orm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Connection {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    private static java.sql.Connection connection;
+
+    public static void createConnection(String dbName, String user, String password) throws SQLException {
+        Properties props = new Properties();
+        props.setProperty("user", user);
+        props.setProperty("password", password);
+        connection = DriverManager.getConnection(DB_URL + dbName, props);
+    }
+
+    public static java.sql.Connection getConnection() {
+        return connection;
+    }
+}
