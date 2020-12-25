373aa9867496827d074d8147b3206656ee7d490d
Boyan Buliev <boyanbot@gmail.com>
2020-12-25 13:21:25 +0200

    Created Spring project

---
+++ Spring Data/SpringDataIntro/src/main/java/com/springdatademo/springdatademo/App.java
@@ -0,0 +1,9 @@
+package com.springdatademo.springdatademo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class App implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}
