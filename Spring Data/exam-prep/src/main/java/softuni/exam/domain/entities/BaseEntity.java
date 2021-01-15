b10fe8cb4ef2fd1f5bd5d8ae39f3764a1e17a3e1
Boyan Buliev <boyanbot@gmail.com>
2021-01-15 16:43:56 +0200

    Minor updates in classes

--- Spring Data/exam-prep/src/main/java/softuni/exam/domain/entities/BaseEntity.java
+++ Spring Data/exam-prep/src/main/java/softuni/exam/domain/entities/BaseEntity.java
@@ -9,16 +9,16 @@ import javax.persistence.MappedSuperclass;
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     public BaseEntity() {
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 }
